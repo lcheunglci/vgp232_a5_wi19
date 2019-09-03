ff15e0ad4ed3e2c0b07f1b3b01d33e1bc774923c
kev l <l-kevin@live.cn>
2019-09-03 12:37:09 -0700

    Kairus -- serialization  //todo //fix // unitest   -- done

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -23,8 +23,27 @@ namespace Assignment5
                 Console.WriteLine(pokemon.Name);
             }
 
+
             // TODO: Add a pokemon bag with 2 bulbsaur, 1 charlizard, 1 mew and 1 dragonite
             // and save it out and load it back and list it out.
+
+            PokemonBag pokemonBag = new PokemonBag();
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("bulbasaur"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("bulbasaur"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("Charizard"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("mew"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("dragonite"));
+
+            string filepath = Environment.CurrentDirectory + "\\PokemonBag.xml";
+            pokemonBag.Save(filepath);
+            PokemonBag pokemonBag2 = pokemonBag.Load(filepath);
+
+            Console.WriteLine();
+            foreach (var bagItem in pokemonBag2.Pokemons)
+            {
+                Console.WriteLine(pokedex.GetPokemonByIndex(bagItem).Name);
+            }
+
 
             Console.ReadKey();
         }
---
+++ Assignment5/Tests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assignment5.Data;
+
+namespace Assignment5
+{
+    class Tests
+    {
+        string myOutputFilename = "PokemonBag.xml";
+        string myInputFilename = "pokemon151.xml";
+        string myTestInputPath = "";
+        string myTestOutputPath = "";
+        string myDirPath;
+
+        [SetUp]
+        public void SetUp()
+        {
+            myDirPath = System.AppContext.BaseDirectory;
+            myTestInputPath = Path.Combine(myDirPath, myInputFilename);
+            myTestOutputPath = Path.Combine(myDirPath, myOutputFilename);
+
+            myDirPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            myDirPath = this.GetType().Assembly.Location;
+            myDirPath = Path.GetDirectoryName(myDirPath);
+        }
+
+
+        [TestCase("Pikachu")]
+        public void SavePokemonBagFile(string name)
+        {
+            PokemonReader reader = new PokemonReader();
+            Pokedex pokedex = reader.Load(myTestInputPath);
+            PokemonBag pokemonBag = new PokemonBag();
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName(name));
+            pokemonBag.Save(myTestOutputPath);
+            Assert.IsTrue(File.Exists(myTestOutputPath));
+
+        }
+        [Test]
+        public void LoadPokemonBagFile()
+        {
+            PokemonReader reader = new PokemonReader();
+            Pokedex pokedex = reader.Load(myTestInputPath);
+            PokemonBag pokemonBag = new PokemonBag();
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("bulbasaur"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("bulbasaur"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("Charizard"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("mew"));
+            pokemonBag.AddPokemon(pokedex.GetPokemonByName("dragonite"));
+            pokemonBag.Save(myTestOutputPath);
+            pokemonBag.Load(myTestOutputPath);
+
+            Assert.AreEqual(5, pokemonBag.Pokemons.Count);
+        }
+
+    }
+}
